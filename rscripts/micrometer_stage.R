9b95af1c0472b24fb848079a23322537c78e69fa
= <=>
2021-05-28 14:57:30 -0400

    considered error along the x axis of the plots

--- rscripts/micrometer_stage.R
+++ rscripts/micrometer_stage.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(ggthemes)
 
-setwd("~/hoffman/capacitance")
+setwd("~/hoffman/capacitance/capacitance_sensing/rscripts")
 
-micrometer_stage3 <- read_csv("micrometerstage4.csv")
+micrometer_stage_sep1 <- read_csv("../csv_files/micrometerstage1.csv")
+micrometer_stage_sep2 <- read_csv("../csv_files/micrometerstage2.csv")
 
-micrometer_stage3 %>% 
+micrometer_stage_sep1 %>% 
   mutate(distance = separation) %>% 
   group_by(distance) %>%
   summarize(avg_cap = mean(capacitance), count = n()) %>%
@@ -20,4 +21,31 @@ micrometer_stage3 %>%
    # geom_point(aes(x= distance,
    #               y= 8.85*5*10^(-4)/(distance/(10^6))+8.7092,
    #               color = "red"), show.legend = FALSE)+
+  geom_smooth(method = "lm")+
   theme_minimal()
+
+trendline_sep1 <- lm(avg_cap/10^6~distance, data =
+                       micrometer_stage_sep1 %>% 
+                       mutate(distance = separation) %>% 
+                       group_by(distance) %>%
+                       summarize(avg_cap = mean(capacitance), count = n()) %>%
+                       filter(count>1) %>% 
+                       ungroup())
+summary(trendline_sep1)
+trendline_sep1
+
+slope_sep <- -0.0009401  
+intercept_sep <- 13.9367222
+
+sep1 <- micrometer_stage_sep1 %>% 
+  mutate(distance = separation) %>% 
+  group_by(distance) %>%
+  summarize(avg_cap = mean(capacitance)/(10^6), count = n()) %>%
+  filter(count>1) %>% 
+  ungroup() %>% 
+  mutate(theoretical_sep = (avg_cap-intercept_sep)/slope_sep) %>% 
+  select(distance, theoretical_sep, avg_cap) %>% 
+  mutate(error = abs(distance - theoretical_sep))
+
+sep1 %>% 
+  summarize(avg_error = mean(error), stdev = sd(error))
--- rscripts/micrometer_stage_area.R
+++ rscripts/micrometer_stage_area.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(ggthemes)
 
-setwd("~/hoffman/capacitance")
+setwd("~/hoffman/capacitance/capacitance_sensing/rscripts")
 
-micrometer_stage_area1 <- read_csv("area1.csv", col_names = c("separation", "capacitance"))
-micrometer_stage_area2 <- read_csv("area2.csv", col_names = c("separation", "capacitance"))
-micrometer_stage_area3 <- read_csv("area3.csv", col_names = c("separation", "capacitance"))
+micrometer_stage_area1 <- read_csv("../csv_files/area1.csv", col_names = c("separation", "capacitance"))
+micrometer_stage_area2 <- read_csv("../csv_files/area2.csv", col_names = c("separation", "capacitance"))
+micrometer_stage_area3 <- read_csv("../csv_files/area3.csv", col_names = c("separation", "capacitance"))
 
 micrometer_stage_area1 %>% 
   mutate(distance = separation) %>% 
@@ -23,6 +23,7 @@ micrometer_stage_area1 %>%
   # geom_point(aes(x= distance,
   #               y= 8.85*5*10^(-4)/(distance/(10^6))+8.7092,
   #               color = "red"), show.legend = FALSE)+
+  geom_smooth(method = "lm")+
   theme_minimal()
 
 micrometer_stage_area2 %>% 
@@ -62,6 +63,86 @@ micrometer_stage_area3 %>%
   # geom_point(aes(x= distance,
   #               y= 8.85*5*10^(-4)/(distance/(10^6))+8.7092,
   #               color = "red"), show.legend = FALSE)+
+  geom_smooth(method = "lm")+
   theme_minimal()
+
+trendline1 <- lm(avg_cap/10^6~distance, data = 
+                   micrometer_stage_area1 %>% 
+                   mutate(distance = separation) %>% 
+                   group_by(distance) %>%
+                   summarize(avg_cap = mean(capacitance), count = n()) %>%
+                   mutate(error = (distance<2300)) %>% 
+                   mutate(distance = distance-error*(2300-distance)/50*500) %>% 
+                   mutate(distance = distance - 2800+28000) %>% 
+                   filter(count>1) %>% 
+                   ungroup())
+trendline1
+
+trendline2 <- lm(avg_cap/10^6~distance, data = 
+                   micrometer_stage_area2 %>% 
+                   mutate(distance = separation) %>% 
+                   group_by(distance) %>%
+                   summarize(avg_cap = mean(capacitance), count = n()) %>%
+                   mutate(error = (distance<2300)) %>% 
+                   mutate(distance = distance-error*(2300-distance)/50*500) %>% 
+                   mutate(distance = distance - 2800+28000) %>% 
+                   filter(count>1) %>% 
+                   ungroup())
+trendline2  
+
+trendline3 <- lm(avg_cap/10^6~distance, data = 
+                   micrometer_stage_area3 %>% 
+                   mutate(distance = separation) %>% 
+                   group_by(distance) %>%
+                   summarize(avg_cap = mean(capacitance), count = n()) %>%
+                   mutate(error = (distance<2300)) %>% 
+                   mutate(distance = distance-error*(2300-distance)/50*500) %>% 
+                   mutate(distance = distance - 2800+28000) %>% 
+                   filter(count>1) %>% 
+                   ungroup())
+trendline3  
+
+summary(trendline1)
+summary(trendline2)
+summary(trendline3)
+
+trendline1
+trendline2
+trendline3
+
+# error analysis
+epsilon <- 8.85*10^(-6) # units of pF/micron
+d <- 2829.56 # units of microns
+h <- 27912 # units of microns
+slope <- 8.73*10^(-5)# units of pF/micron
+intercept <- 8.829 # units of pF
+
+area1 <- micrometer_stage_area1 %>% 
+  mutate(distance = separation) %>% 
+  group_by(distance) %>%
+  summarize(avg_cap = mean(capacitance)/10^6, count = n()) %>%
+  filter(count>1) %>% 
+  ungroup() %>%
+  mutate(width = distance - 2800+28000) %>% 
+# Note that at this point all units are microns and pF
+  mutate(theoretical_width = (avg_cap-intercept)*d/epsilon/h) %>% 
+  mutate(theoretical_width2 = (avg_cap-intercept)/slope) %>% 
+  select(width, theoretical_width, theoretical_width2, avg_cap) %>% 
+  mutate(error = abs(width - theoretical_width)) %>% 
+  mutate(error2 = abs(width - theoretical_width))
+
+area1 %>% 
+  summarize(avg_error = mean(error), stdev = sd(error))
+
+area1 %>% 
+  summarize(avg_error2 = mean(error2), stdev2 = sd(error2))
+
+area1 %>% 
+  ggplot(aes(error)) +
+  geom_histogram()+
+  theme_minimal()
+
+  
+  
 
   
