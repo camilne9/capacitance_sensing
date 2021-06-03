a8257539e28c4ecd1f05bff7685182083eab808a
= <=>
2021-06-03 14:16:08 -0400

    script for calculating error when passed a data set

---
+++ rscripts/general_error_calc.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(ggthemes)
+
+setwd("~/hoffman/capacitance/capacitance_sensing/rscripts")
+
+# I read in the data
+general_error <- read_csv("../csv_files/differential_mode3.csv", 
+                          col_names = c("distance", "capacitance"))
+                          
+general_error <- general_error %>% 
+  mutate(distance = distance+1) %>% 
+  group_by(distance) %>%
+  summarize(avg_cap = mean(capacitance)/(10^6), count = n()) %>%
+  filter(count>1) %>% 
+  ungroup() %>% 
+  mutate(reciprocal = 1/distance)
+
+#For Reciprocal
+slope <- coef(lm(avg_cap~reciprocal, general_error))[["reciprocal"]]
+intercept <- coef(lm(avg_cap~reciprocal, general_error))[["(Intercept)"]]
+
+general_error %>% 
+  mutate(theory_value = (avg_cap-intercept)/slope) %>% 
+  mutate(error = abs(1/reciprocal-1/theory_value)) %>% 
+  summarize(avg_error = mean(error), stdev = sd(error))
+
+#For linear
+slope <- coef(lm(avg_cap~distance, general_error))[["distance"]]
+intercept <- coef(lm(avg_cap~distance, general_error))[["(Intercept)"]]
+
+general_error %>% 
+  mutate(theory_value = (avg_cap-intercept)/slope) %>% 
+  mutate(error = abs(distance-theory_value)) %>% 
+  summarize(avg_error = mean(error), stdev = sd(error))
+
