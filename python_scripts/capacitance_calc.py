49598aa07054f68fbe89e405cbe5bd3920291c47
= <=>
2021-06-09 11:50:35 -0400

    created function that find theoretical slope that should be seen in the capacitance data

--- python_scripts/capacitance_calc.py
+++ python_scripts/capacitance_calc.py
@@ -17,6 +17,35 @@ def clean_readings():
 def avg_cap(caps):
     return sum(caps)/len(caps)
 
+def theoretical_slope(height, separation, width = None, height_units="mm", separation_units = "microns", width_units = "mm"):
+    epsilon = 8.85 # with units of pF/m
+    if width is None:
+        slope = epsilon*height/separation
+        scale_factor = convert_to_meters(height_units)/convert_to_microns(separation_units)
+        slope = slope*scale_factor
+        #if there's no width, then the width is the changing parameter
+    else:
+        slope = epsilon*height*width/(separation**2)
+        scale_factor = convert_to_meters(height_units)*convert_to_meters(width_units)/ \
+            (convert_to_microns(separation_units)*convert_to_meters(separation_units))
+        slope = slope*scale_factor
+        # if there is a width, then we're changing the separation distance
+    return slope
+    # the returned value is the slope in pF/micron
 
-print(avg_cap(clean_readings()))
-print(statistics.pstdev(clean_readings()))
+def convert_to_meters(input_unit):
+    unit = ["mm", "microns", "m", "in"]
+    meter_factors = [10**(-3), 10**(-6), 1, 0.0254] #these conversion factors convert to m
+    return meter_factors[unit.index(input_unit)]
+
+def convert_to_microns(input_unit):
+    unit = ["mm", "microns", "m", "in"]
+    micron_factors = [10**(3), 1, 10**(6), 0.0254*10**(6)] #these conversion factors convert to m
+    return micron_factors[unit.index(input_unit)]
+
+# print(avg_cap(clean_readings()))
+# print(statistics.pstdev(clean_readings()))
+print(f"diff, area: {theoretical_slope(15, 100)}")
+print(f"sing, area: {theoretical_slope(15, 200)}")
+print(f"diff, sep: {theoretical_slope(15, 150, 15)}")
+print(f"sing, sep: {theoretical_slope(15, 150, 10)}")
