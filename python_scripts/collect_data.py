d59151d0ac5472691d6ebc39bd7c608eb36e1a58
= <=>
2021-07-09 14:10:59 -0400

    allows ramping voltage and has safegaurd for overwriting files

--- python_scripts/collect_data.py
+++ python_scripts/collect_data.py
@@ -14,31 +14,48 @@ volt = serial.Serial("/dev/tty.usbmodem14101" , 9600)
 # Tunable constants
 readings_per_batch = 100
 max_voltage = 175
-trials = 5
+trials = 15
 wait_time = 5
 offset = 128
+voltages = [0, 25, 50, 75, 100, 125, 150, 175,
+            150, 125, 100, 75, 50, 25]
+            # 0, 25, 50, 75, 100, 125, 150, 175,
+            # 150, 125, 100, 75, 50, 25,
+            # 0, 25, 50, 75, 100, 125, 150, 175,
+            # 150, 125, 100, 75, 50, 25, 0]
 
 # data_array = ["batch", "voltage", "time", "raw", "capacitance"]
 data_array = []
 
-def collect_data(readings_per_batch, max_voltage, trials, wait_time, offset):
+def collect_data(readings_per_batch, max_voltage, trials, wait_time, offset, voltages = False):
+    if voltages:
+        trials = len(voltages)
     for trial in range(trials):
-        print("going to sleep")
-        time.sleep(3)
-        print("waking up")
-        if trial%2 == 0:
-            print("zero volts")
-            volt.write(b"V0;")
+        if not voltages:
+            print("no voltages")
+            if trial%2 == 0:
+                print("zero volts")
+                voltage = 0
+                volt.write(b"V%d;"%voltage)
+            else:
+                print("max voltage")
+                voltage = max_voltage
+                volt.write(b"V%d;"%voltage)
         else:
-            print("max voltage")
-            volt.write(b"V175;")
+            voltage = voltages[trial]
+            print(voltage)
+            volt.write(b"V%d;"%voltage)
+
+        print("going to sleep")
+        time.sleep(1)
+        print("waking up")
 
         for interation in range(readings_per_batch):
             cap.write(b"c;")
             val = cap.readline()
             raw = clean_val(val)
             capacitance = convert_to_cap(raw, offset)
-            data_array.append([trial, max_voltage*(trial%2), (trial-1)*wait_time, raw, capacitance])
+            data_array.append([trial, voltage, (trial)*wait_time, raw, capacitance])
     return data_array
 
 def clean_val(val):
@@ -50,7 +67,7 @@ def clean_val(val):
             char_list.remove(character)
     # print(char_list)
     val = int("".join(char_list))
-    print(val)
+    # print(val)
     # print("clean")
     return val
 
@@ -65,12 +82,23 @@ def save_to_csv(data, filename):
     np.savetxt(filename, data_array, delimiter=",", fmt='%s')
     return
 
-def main(filename, readings_per_batch, max_voltage, trials, wait_time, offset):
+def main(filename, readings_per_batch, max_voltage, trials, wait_time, offset, voltages = False):
+    if path.exists(filename):
+        proceed = input("file exist, are you sure you want to overwrite? (y/n): ")
+        if proceed != "y":
+            print("exit:")
+            print(proceed)
+            exit()
+        else:
+            print("Proceed:")
+            print(proceed)
+    cap.write(b"D%d;\n"%offset)
+    cap.readline()
     volt.write(b"O;")
-    save_to_csv(collect_data(readings_per_batch, max_voltage, trials, wait_time, offset), filename)
+    save_to_csv(collect_data(readings_per_batch, max_voltage, trials, wait_time, offset, voltages), filename)
     volt.write(b"V0;")
     volt.write(b"F;")
     return
 
-filename = "csv_files/7-09_thor1_se.csv"
-main(filename, readings_per_batch, max_voltage, trials, wait_time, offset)
+filename = "csv_files/7-09_thor8_se.csv"
+main(filename, readings_per_batch, max_voltage, trials, wait_time, 255, voltages)
