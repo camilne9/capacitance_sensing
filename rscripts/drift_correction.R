23bdaa0f03c5ab225af03d84dfb6f25657b24290
= <=>
2021-07-09 11:16:32 -0400

    small changes to r scripts

--- rscripts/drift_correction.R
+++ rscripts/drift_correction.R
@@ -4,10 +4,12 @@ library(ggthemes)
 setwd("~/hoffman/capacitance/capacitance_sensing/rscripts")
 
 # I read in the data
-drift_data <- read_csv("../csv_files/drift_test1_6-21.csv", 
-                          col_names = c("distance", "capacitance", "batch"))
+drift_data <- read_csv("../csv_files/7-08_thor4_se.csv",
+                       col_names = c("distance", "raw", "batch", "capacitance"))
 # max_voltage_batch <- c(2,4,6,8,10,12,14)
-max_voltage_batch <- c(4:6, 10:12, 16:18, 22:24, 28:30)
+max_voltage_batch <- c(4:6, 10:12, 16:18, 22:24, 28:30, 34:36, 40:42, 46:48)
+# max_voltage_batch <- c(4:6, 10:12, 16:21, 25:27)
+# max_voltage_batch <- c(4:6, 13:18, 22:24, 28:30, 34:36)
 # max_voltage_batch = c(13:18, 25:30, 37:42, 49:54)
 # max_voltage_batch = c(7:9, 13:15, 19:21)
 # max_voltage_batch = c(55:57, 61:63, 67:69)
@@ -15,17 +17,20 @@ max_voltage_batch <- c(4:6, 10:12, 16:18, 22:24, 28:30)
 # max_voltage_batch <- c(9:12, 17:20)
 # max_voltage_batch <- c(7:9, 13:15, 19:21, 25:27, 31:33, 37:39)
 max_voltage <- 175
-time_interval <- 30
+time_interval <- 20
 
 clean_drift_data <- drift_data %>% 
   group_by(distance, batch) %>%
-  summarize(avg_cap = mean(capacitance), count = n()) %>%
+  summarize(avg_cap = mean(capacitance), stdev =sd(capacitance), count = n()) %>%
+  # summarize(avg_cap = mean(capacitance), count = n(), Voltage = mean(Voltage)) %>%
   filter(count>1) %>% 
+  # filter(distance <=1000) %>% 
   # filter(batch < 25) %>% 
   ungroup() %>% 
   mutate(Voltage = as.factor(max_voltage*(batch %in% max_voltage_batch))) %>% 
+  # mutate(Voltage = as.factor(Voltage)) %>% 
   mutate(time = time_interval*(batch-1)) %>% 
-  select(batch, time, avg_cap, Voltage)
+  select(batch, time, avg_cap, Voltage, stdev)
 
 null_slope <- coef(lm(avg_cap~time, 
                       subset(clean_drift_data, Voltage == 0)))[["time"]]
@@ -38,12 +43,12 @@ clean_drift_data %>%
   mutate(drift_adjusted_capacitance = avg_cap-null_slope*time) %>% 
   ggplot(aes(time, avg_cap, color = Voltage))+
   geom_point()+
-  labs(title = "Testing Stray Capacitance by Varying Voltage with Piezo Removed",
-       subtitle ="Alternating 0V and 175V, Single-Ended Measurements, Unmoving Electrodes",
-       caption = "Data File: drift_test1_6-21.csv \n
-       175V: 4:6, 10:12, 16:18, 22:24, 28:30",
+  # geom_errorbar(aes(ymin=(avg_cap-2*stdev), ymax=(avg_cap+2*stdev)), width=.2)+
+  labs(title = "Capacitance by Varying Voltage",
+       subtitle ="Alternating 0V and 175V, Single-Ended, New Thorlabs Stack, D255",
+       caption = "Data File: 7-08_thor4_se.csv \n
+       175V: 4:6, 10:12, 16:18, 22:24, 28:30, 34:36, 40:42, 46:48",
        x = "Time (s)",
        y = "Capacitance (pF)")+
   # geom_smooth(method = "lm")+
   theme_minimal()
-
--- rscripts/quick_plot.R
+++ rscripts/quick_plot.R
@@ -3,26 +3,31 @@ library(ggthemes)
 
 setwd("~/hoffman/capacitance/capacitance_sensing/rscripts")
 
-quick_data <- read_csv("../csv_files/7-08_thor1_se.csv",
+quick_data <- read_csv("../csv_files/7-09_thor1_se.csv",
                        col_names = c("distance", "raw", "batch", "capacitance"))
-
-quick_data %>% 
-  group_by(distance, batch) %>%
+temp_data <- read_csv("../csv_files/7-09_thor1_se.csv",
+                       col_names = c("batch", "Voltage", "time", "raw", "capacitance"))
+temp_data %>% 
+  group_by(batch) %>%
   summarize(avg_cap = mean(capacitance), 
-            avg_raw = mean(raw),stdev =sd(capacitance), count = n()) %>%
+            avg_raw = mean(raw),stdev =sd(capacitance), count = n(), 
+            Voltage = as.factor(mean(Voltage))) %>%
   filter(count>1) %>% 
   ungroup() %>%
-  # mutate(voltage = as.factor(ifelse(7>batch, "0V", 
-  #                                   ifelse(batch>12, "0V", "175V")))) %>% 
+# the following 3 lines apply if shearing and calibrating 
+  # mutate(time = distance) %>%
+  # mutate(distance = 10*floor(distance/40)) %>%
+  # mutate(Voltage = as.factor(175*((batch-1)%%2))) %>%
+
   # mutate(voltage = as.factor(((batch-1)%%2)*175)) %>% 
   # ggplot(aes(distance, avg_cap/10^6, color = voltage))+
-  ggplot(aes(10*(batch-1), avg_cap))+
+  ggplot(aes(batch, avg_cap, color = Voltage))+
   geom_point()+
   # geom_errorbar(aes(ymin=(avg_cap-2*stdev), ymax=(avg_cap+2*stdev)), width=.2)+
-  labs(title = "Shearing and Calibration of New Thorlabs Stack",
-       subtitle ="New Thorlabs Stack, Single-Ended, D255",
-       caption ="Data File: 7-08_thor1_se.csv",
-       x = "Time (s)",
+  labs(title = "Shearing and Calibration of Thorlabs Stack",
+       subtitle ="Shearing Closer, Single-Ended, D128",
+       caption ="Data File: 7-08_thor6_se.csv",
+       x = "Displacement (microns)",
        y = "Capacitance (pF)")+
   # geom_smooth(method = "lm")+
   theme_minimal()
