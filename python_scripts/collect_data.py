7c823cfd6a642d88755923df892232d706163b73
= <=>
2021-07-14 11:13:53 -0400

    reformatted as classes to allow for synergy between files later

--- python_scripts/collect_data.py
+++ python_scripts/collect_data.py
@@ -6,141 +6,144 @@ import numpy as np
 import os.path
 from os import path
 
-# create objects for the capacitance sensor and voltage source. The path
-# name is generally device specific.
-cap = serial.Serial("/dev/tty.usbserial-D30A2ZWX" , 9600)
-volt = serial.Serial("/dev/tty.usbmodem14101" , 9600)
-
-# data_array = ["batch", "voltage", "time", "raw", "capacitance"]
-data_array = []
-
-def collect_data(readings_per_batch, max_voltage, trials, wait_time, pause = False, offset = 255, voltages = False):
+class Collect_Data():
     """
-    "readings_per_batch" is an int indicating how many distinct capacitance
-        measurements are taken with each configuration
-    "max_voltage" is an int from 0 to 175 indicating the largest voltage
-        passed to induce the shearing
-    "trials" is an int inidcating how many batches are to be taken. This
-        variable is overridden (and therefore meaningless) if the "voltages"
-        variable is not set to False.
-    "wait_time" is a float that decides how long of a time gap between setting
-        the voltage to max and when collection starts. This number can be quite
-        small unless there's strong reason against it
-    "pause" is an boolean variable deciding if the script proceeds
-        automatically or whether it pauses. If moving the electrode positions
-        between batches, set this to True to have time for that adjustment. If
-        leaving the system as it is, leave as False (default) to just have the
-        script simply run through
-    "offset" is an int from 128 to 255. This sets the offset of the
-        capacitance sensor. A larger offset allows a closer separation distance
-    "voltages" controls whether you ramp a set of voltages or if you toggle
-        between a min and a max voltage. If voltages is 'False' (default) then
-        the voltage will toggle. If you pass a list, then the voltage will be
-        set to each entry of the list in succession.
+    A class for data collection
     """
-    # overrides trials number if we're ramping voltages
-    if voltages:
-        trials = len(voltages)
-    # we loop through the data collection process as many times as we require
-    # for the given number of trials
-    for trial in range(trials):
-        # if we are toggling voltage, we set the voltage based on the parity
-        # of the batch number
-        if not voltages:
-            if trial%2 == 0:
-                print("zero volts")
-                voltage = 0
-                volt.write(b"V%d;"%voltage)
-                if pause:
-                    input("Press 'enter' when you're ready to proceed")
+
+    def __init__(self, filename, readings_per_batch, max_voltage, trials, wait_time, pause, offset, voltages = False):
+        # create objects for the capacitance sensor and voltage source. The path
+        # name is generally device specific.
+        self.cap = serial.Serial("/dev/tty.usbserial-D30A2ZWX" , 9600)
+        self.volt = serial.Serial("/dev/tty.usbmodem14101" , 9600)
+
+        # self.data_array = ["batch", "voltage", "time", "raw", "capacitance"]
+        self.data_array = []
+
+        # Safeguard to avoid accidentally overwriting files
+        if path.exists(filename):
+            proceed = input("file exist, are you sure you want to overwrite? (y/n): ")
+            if proceed != "y":
+                print("Exiting.")
+                self.volt.write(b"V0;")
+                self.volt.write(b"F;")
+                exit()
             else:
-                print("max voltage")
-                voltage = max_voltage
-                volt.write(b"V%d;"%voltage)
-        # if we're ramping a set of voltages, we set the voltage according to
-        # the set passed
-        else:
-            voltage = voltages[trial]
-            print(voltage)
-            volt.write(b"V%d;"%voltage)
+                print("Proceeding.")
+        # Sets capacitance offset, turns on voltage source, and creates timestamp
+        self.cap.write(b"D%d;\n"%offset)
+        self.cap.readline()
+        self.volt.write(b"O;")
+        self.create_absolute_timestamp()
+        # Collects data and saves it to csv
+        self.save_to_csv(self.collect_data(readings_per_batch, max_voltage, trials, wait_time, pause, offset, voltages), filename)
+        # turns off voltage source
+        self.volt.write(b"V0;")
+        self.volt.write(b"F;")
+        return
 
-        # brief pause as the stack shears and comes to rest
-        print("going to sleep")
-        time.sleep(wait_time)
-        print("waking up")
+    def collect_data(self, readings_per_batch, max_voltage, trials, wait_time, pause = False, offset = 255, voltages = False):
+        """
+        "readings_per_batch" is an int indicating how many distinct capacitance
+            measurements are taken with each configuration
+        "max_voltage" is an int from 0 to 175 indicating the largest voltage
+            passed to induce the shearing
+        "trials" is an int inidcating how many batches are to be taken. This
+            variable is overridden (and therefore meaningless) if the "voltages"
+            variable is not set to False.
+        "wait_time" is a float that decides how long of a time gap between setting
+            the voltage to max and when collection starts. This number can be quite
+            small unless there's strong reason against it
+        "pause" is an boolean variable deciding if the script proceeds
+            automatically or whether it pauses. If moving the electrode positions
+            between batches, set this to True to have time for that adjustment. If
+            leaving the system as it is, leave as False (default) to just have the
+            script simply run through
+        "offset" is an int from 128 to 255. This sets the offset of the
+            capacitance sensor. A larger offset allows a closer separation distance
+        "voltages" controls whether you ramp a set of voltages or if you toggle
+            between a min and a max voltage. If voltages is 'False' (default) then
+            the voltage will toggle. If you pass a list, then the voltage will be
+            set to each entry of the list in succession.
+        """
+        # overrides trials number if we're ramping voltages
+        if voltages:
+            trials = len(voltages)
+        # we loop through the data collection process as many times as we require
+        # for the given number of trials
+        for trial in range(trials):
+            # if we are toggling voltage, we set the voltage based on the parity
+            # of the batch number
+            if not voltages:
+                if trial%2 == 0:
+                    print("zero volts")
+                    voltage = 0
+                    self.volt.write(b"V%d;"%voltage)
+                    if pause:
+                        input("Press 'enter' when you're ready to proceed")
+                else:
+                    print("max voltage")
+                    voltage = max_voltage
+                    self.volt.write(b"V%d;"%voltage)
+            # if we're ramping a set of voltages, we set the voltage according to
+            # the set passed
+            else:
+                voltage = voltages[trial]
+                print(voltage)
+                self.volt.write(b"V%d;"%voltage)
 
-        # collects as many measurements as indicated. Cleans the raw reading
-        # so that it is just the int and also converts it to a capacitance.
-        # All the information is stored in a list of lists
-        for interation in range(readings_per_batch):
-            cap.write(b"c;")
-            val = cap.readline()
-            raw = clean_val(val)
-            capacitance = convert_to_cap(raw, offset)
-            data_array.append([trial, voltage, time.time() - absolute_timestamp, raw, capacitance])
-    return data_array
+            # brief pause as the stack shears and comes to rest
+            print("going to sleep")
+            time.sleep(wait_time)
+            print("waking up")
 
-def clean_val(val):
-    """
-    Parses readings from the capacitance sensor to isolate the actual
-    capacitance information.
-    """
-    char_list = list(str(val))
-    for character in list(str(val)):
-        if not character.isdigit():
-            char_list.remove(character)
-    val = int("".join(char_list))
-    return val
+            # collects as many measurements as indicated. Cleans the raw reading
+            # so that it is just the int and also converts it to a capacitance.
+            # All the information is stored in a list of lists
+            for interation in range(readings_per_batch):
+                self.cap.write(b"c;")
+                val = self.cap.readline()
+                raw = self.clean_val(val)
+                capacitance = self.convert_to_cap(raw, offset)
+                self.data_array.append([trial, voltage, time.time() - self.absolute_timestamp, raw, capacitance])
+        return self.data_array
 
-def convert_to_cap(datapoint, offset):
-    """
-    Takes raw reading from the AD7746 capacitance sensor and converts it to an
-    actual capacitance. (WARNING: conversion is imperfect.) Datapoint and
-    offset are each ints.
-    """
-    datapoint = (datapoint)*(4.8828125e-7) - 4.096 + (offset - 128)*(.148)
-    return datapoint
+    def clean_val(self, val):
+        """
+        Parses readings from the capacitance sensor to isolate the actual
+        capacitance information.
+        """
+        char_list = list(str(val))
+        for character in list(str(val)):
+            if not character.isdigit():
+                char_list.remove(character)
+        val = int("".join(char_list))
+        return val
 
-def save_to_csv(data, filename):
-    """
-    Stores the collected data (the "data" argument which takes a list of lists)
-    in a csv file whose name is given by the str argument "filename".
-    """
-    data_array = np.asarray(data)
-    np.savetxt(filename, data_array, delimiter=",", fmt='%s')
-    return
+    def convert_to_cap(self, datapoint, offset):
+        """
+        Takes raw reading from the AD7746 capacitance sensor and converts it to an
+        actual capacitance. (WARNING: conversion is imperfect.) Datapoint and
+        offset are each ints.
+        """
+        datapoint = (datapoint)*(4.8828125e-7) - 4.096 + (offset - 128)*(.148)
+        return datapoint
 
-def create_absolute_timestamp():
-    """
-    Gets current timestamp to allow the time variable to be measured in
-    seconds relative to this starting point
-    """
-    global absolute_timestamp
-    absolute_timestamp = time.time()
+    def save_to_csv(self, data, filename):
+        """
+        Stores the collected data (the "data" argument which takes a list of lists)
+        in a csv file whose name is given by the str argument "filename".
+        """
+        self.data_array = np.asarray(data)
+        np.savetxt(filename, self.data_array, delimiter=",", fmt='%s')
+        return
 
-def main(filename, readings_per_batch, max_voltage, trials, wait_time, pause, offset, voltages = False):
-    # Safeguard to avoid accidentally overwriting files
-    if path.exists(filename):
-        proceed = input("file exist, are you sure you want to overwrite? (y/n): ")
-        if proceed != "y":
-            print("Exiting.")
-            volt.write(b"V0;")
-            volt.write(b"F;")
-            exit()
-        else:
-            print("Proceeding.")
-            print(proceed)
-    # Sets capacitance offset, turns on voltage source, and creates timestamp
-    cap.write(b"D%d;\n"%offset)
-    cap.readline()
-    volt.write(b"O;")
-    create_absolute_timestamp()
-    # Collects data and saves it to csv
-    save_to_csv(collect_data(readings_per_batch, max_voltage, trials, wait_time, pause, offset, voltages), filename)
-    # turns off voltage source
-    volt.write(b"V0;")
-    volt.write(b"F;")
-    return
+    def create_absolute_timestamp(self):
+        """
+        Gets current timestamp to allow the time variable to be measured in
+        seconds relative to this starting point
+        """
+        self.absolute_timestamp = time.time()
 
 
 # Tunable constants
@@ -155,8 +158,13 @@ voltages = [0, 25, 50, 75, 100, 125, 150, 175,
             150, 125, 100, 75, 50, 25]
             # 0, 25, 50, 75, 100, 125, 150, 175,
             # 150, 125, 100, 75, 50, 25, 0]
-filename = "csv_files/7-13_5m9.csv"
+filename = "csv_files/7-14_test2.csv"
+
+def main(filename, readings_per_batch, max_voltage, trials, wait_time,
+            pause, offset, voltages=False):
+        Collect_Data(filename, readings_per_batch, max_voltage, trials,
+                wait_time, pause, offset, voltages)
 
 main(filename=filename, readings_per_batch=readings_per_batch,
         max_voltage=max_voltage, trials=trials, wait_time=wait_time,
-        pause=True, offset=225, voltages=False)
+        pause=False, offset=225, voltages=False)
--- python_scripts/find_sep_dist.py
+++ python_scripts/find_sep_dist.py
@@ -8,151 +8,161 @@ from os import path
 import scipy.optimize as optimize
 import matplotlib.pyplot as plt
 
-ep = 8.85
 
-def load_csv(filename):
+class Shear_Distance():
     """
-    Accesses the named csv file and puts the data in a list of lists. Converts
-    the str variable entries to ints and floats. The required argument is the
-    filepath for the csv file being accessed.
+    Class for calculating shear distance, overlapping area, and initial
+    separation distance.
     """
-    with open(filename) as csvfile:
-        data_array = list(csv.reader(csvfile))
-    float_array = []
-    for row in data_array:
-        float_array.append(list(map(float, row)))
-    return float_array
+    def __init__(self, filename, area, separation, plot):
+        """
+        Executes appropriate methods in the correct order. Takes one required
+        argument which must be the name of the csv file containing the relevant
+        data and one optional argument to indicate if you want the data plotted.
+        """
+        self.ep = 8.85
+        # creates variable "self.data" that is a list of lists containing
+        # all of the information from the named csv file
+        self.data = self.avg_data(self.load_csv(filename))
+        # creates variables "self.area" and "self.separation" which indicate
+        # the error minimizing overlapping area and initial electrode
+        # separation distance respectively unless values are already passed
+        if area == None or separation == None:
+            self.area, self.separation = self.find_area_and_sep()
+        else:
+            self.area = area
+            self.separation = separation
+        # creates variable for the error minimizing shear distance by taking
+        # the area and separation distance as given
+        self.shear_distance = self.find_shear_distance()
+        # prints the findins and plots the data
+        self.print_results()
+        if plot:
+            self.plot_data(self.data)
 
-def avg_data(data_array):
-    """"
-    Groups the data by batch and averages the capacitance readings. Takes one
-    argument which is a list of lists containing all of the data in the format
-    its in as it is loaded in "load_csv()".
-    """"
-    max_batch = int(int(10*float(data_array[-1][0]))/10)
-    max_batch += 1
-    avg_data = []
-    for batch in range(max_batch):
-        temp_filtered_array = []
+    def load_csv(self, filename):
+        """
+        Accesses the named csv file and puts the data in a list of lists. Converts
+        the str variable entries to ints and floats. The required argument is the
+        filepath for the csv file being accessed.
+        """
+        with open(filename) as csvfile:
+            data_array = list(csv.reader(csvfile))
+        float_array = []
         for row in data_array:
-            if int(int(10*float(row[0]))/10) == batch:
-                temp_filtered_array.append(row)
-        avg_data.append(np.mean(np.array(temp_filtered_array), axis = 0).tolist())
-    return avg_data
+            float_array.append(list(map(float, row)))
+        return float_array
+
+    def avg_data(self, data_array):
+        """
+        Groups the data by batch and averages the capacitance readings. Takes one
+        argument which is a list of lists containing all of the data in the format
+        its in as it is loaded in "load_csv()".
+        """
+        max_batch = int(int(10*float(data_array[-1][0]))/10)
+        max_batch += 1
+        avg_data = []
+        for batch in range(max_batch):
+            temp_filtered_array = []
+            for row in data_array:
+                if int(int(10*float(row[0]))/10) == batch:
+                    temp_filtered_array.append(row)
+            avg_data.append(np.mean(np.array(temp_filtered_array), axis = 0).tolist())
+        return avg_data
 
 
-def cap_formula(params):
-    """
-    Finds the sum of the squared errors between the experimental and
-    theoretical capacitance values. Takes one required argument that is a
-    list of parameters [area, separation] which are the overlapping area and
-    initial separation distance.
-    """
-    # filters the data for only cases were the stack was NOT sheared
-    zero_voltage = []
-    for row in data:
-        if row[1] == 0:
-            zero_voltage.append(row)
+    def cap_formula(self, params):
+        """
+        Finds the sum of the squared errors between the experimental and
+        theoretical capacitance values. Takes one required argument that is a
+        list of parameters [area, separation] which are the overlapping area and
+        initial separation distance.
+        """
+        # filters the data for only cases were the stack was NOT sheared
+        zero_voltage = []
+        for row in self.data:
+            if row[1] == 0:
+                zero_voltage.append(row)
 
-    area, separation = params
-    # sums the squared error for each of the sheared cases
-    error = 0
-    for row in zero_voltage:
-        error += (row[-1]-ep*area/(separation+5*row[0]))**2
-    return error
+        area, separation = params
+        # sums the squared error for each of the sheared cases
+        error = 0
+        for row in zero_voltage:
+            error += (row[-1]-self.ep*area/(separation+5*row[0]))**2
+        return error
 
-def shear(x):
-    """
-    Finds the sum of the squared errors between the experimental and
-    theoretical capacitance values. Takes one required argument that is
-    the shear distance of the stack.
-    """
-    # filters the data for only cases were the stack was sheared
-    shear_voltage = []
-    for row in data:
-        if row[1] != 0:
-            shear_voltage.append(row)
-    # sums the squared error for each of the sheared cases
-    error = 0
-    for row in shear_voltage:
-        error += (row[-1]-ep*area/(separation+x+5*(row[0]-1)))**2
-    return error
+    def shear(self, x):
+        """
+        Finds the sum of the squared errors between the experimental and
+        theoretical capacitance values. Takes one required argument that is
+        the shear distance of the stack.
+        """
+        # filters the data for only cases were the stack was sheared
+        shear_voltage = []
+        for row in self.data:
+            if row[1] != 0:
+                shear_voltage.append(row)
+        # sums the squared error for each of the sheared cases
+        error = 0
+        for row in shear_voltage:
+            error += (row[-1]-self.ep*self.area/(self.separation+x+5*(row[0]-1)))**2
+        return error
 
-def find_area_and_sep():
-    """
-    Finds the overlapping area and initial separation distance that minimizes
-    the error in experimental and theoretical capaticance values
-    """
-    # repeatedly runs "cap_formula" with different trial area and separation
-    # to determine the minimizing case
-    initial_guess = [500, 300]
-    result = optimize.minimize(cap_formula, initial_guess)
-    # once the minimum is found, the results are returned. Otherwise, there is
-    # an error message
-    if result.success:
-        fitted_params = result.x
-        return fitted_params
-    else:
-        raise ValueError(result.message)
+    def find_area_and_sep(self):
+        """
+        Finds the overlapping area and initial separation distance that minimizes
+        the error in experimental and theoretical capaticance values
+        """
+        # repeatedly runs "cap_formula" with different trial area and separation
+        # to determine the minimizing case
+        initial_guess = [500, 300]
+        result = optimize.minimize(self.cap_formula, initial_guess)
+        # once the minimum is found, the results are returned. Otherwise, there is
+        # an error message
+        if result.success:
+            fitted_params = result.x
+            return fitted_params
+        else:
+            raise ValueError(result.message)
 
-def find_shear_distance():
-    """
-    Finds the shear distance that minimizes the error in experimental and
-    theoretical capaticance values after taking the area and separation
-    distance as given by the global variables.
-    """
-    # repeatedly runs "cap_formula" with different trial area and separation
-    # to determine the minimizing case
-    initial_guess = [1]
-    result = optimize.minimize(shear, initial_guess)
-    # once the minimum is found, the results are returned. Otherwise, there is
-    # an error message
-    if result.success:
-        fitted_params = result.x
-        return fitted_params[0]
-    else:
-        raise ValueError(result.message)
+    def find_shear_distance(self):
+        """
+        Finds the shear distance that minimizes the error in experimental and
+        theoretical capaticance values after taking the area and separation
+        distance as given by the global variables.
+        """
+        # repeatedly runs "cap_formula" with different trial area and separation
+        # to determine the minimizing case
+        initial_guess = [1]
+        result = optimize.minimize(self.shear, initial_guess)
+        # once the minimum is found, the results are returned. Otherwise, there is
+        # an error message
+        if result.success:
+            fitted_params = result.x
+            return fitted_params[0]
+        else:
+            raise ValueError(result.message)
 
-def print_results():
-    """
-    Prints the calculated global variables of the electrodes' overlapping area,
-    the intial separation distance, and the shear distance of the stack.
-    """
-    print(f"Area: {area}")
-    print(f"Intitial Separation: {separation}")
-    print(f"Shear Distance: {shear_distance}")
+    def print_results(self):
+        """
+        Prints the calculated global variables of the electrodes' overlapping area,
+        the intial separation distance, and the shear distance of the stack.
+        """
+        print(f"Area: {self.area}")
+        print(f"Intitial Separation: {self.separation}")
+        print(f"Shear Distance: {self.shear_distance}")
 
-def main(filename):
-    """
-    Executes appropriate methods in the correct order. Takes one required
-    argument which must be the name of the csv file containing the relevant
-    data and one optional argument to indicate if you want the data plotted.
-    """
-    # creates global variable "data" that is a list of lists containing
-    # all of the information from the named csv file
-    global data
-    data = avg_data(load_csv(filename))
-    # creates global variables "area" and "separation" which indicate the error
-    # minimizing overlapping area and initial electrode separation distance
-    # respectively
-    global area, separation
-    area, separation = find_area_and_sep()
-    # creates global variable for the error minimizing shear distance by taking
-    # the global area and separation distance as given
-    global shear_distance
-    shear_distance = find_shear_distance()
-    # prints the findins and plots the data
-    print_results()
-    plot_data(data)
+    def plot_data(self, data_array):
+        """
+        Plots the data as batch (x axis) vs capacitance (y axis). Required argument
+        is a list of lists with batch number as the first column and capacitance as
+        the last (which is the format done by collect_data.py)
+        """
+        print("plotting...")
+        plt.scatter(np.array(data_array)[:, 0].tolist(), np.array(data_array)[:, -1].tolist())
+        plt.show()
 
-def plot_data(data_array):
-    """
-    Plots the data as batch (x axis) vs capacitance (y axis). Required argument
-    is a list of lists with batch number as the first column and capacitance as
-    the last (which is the format done by collect_data.py)
-    """
-    print("plotting...")
-    plt.scatter(np.array(data_array)[:, 0].tolist(), np.array(data_array)[:, -1].tolist())
-    plt.show()
+def main(filename, area = None, separation = None, plot = True):
+    Shear_Distance(filename, area, separation, plot)
 
-main("csv_files/7-13_5m9.csv")
+main("csv_files/7-13_5m8.csv", plot = False)
