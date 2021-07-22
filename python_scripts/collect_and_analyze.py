4e53e122a3a1ced097fc25c8f22bed664984a2b8
= <=>
2021-07-22 07:45:45 -0400

    added step size argument

--- python_scripts/collect_and_analyze.py
+++ python_scripts/collect_and_analyze.py
@@ -14,23 +14,24 @@ from find_sep_dist import Shear_Distance
 # Tunable constants
 readings_per_batch = 100
 max_voltage = 175
-trials = 6
-wait_time = 1
-offset = 128
+trials = 30
+wait_time = .5
+offset = 220
 voltages = [0, 25, 50, 75, 100, 125, 150, 175,
             150, 125, 100, 75, 50, 25,
             0, 25, 50, 75, 100, 125, 150, 175,
             150, 125, 100, 75, 50, 25]
             # 0, 25, 50, 75, 100, 125, 150, 175,
             # 150, 125, 100, 75, 50, 25, 0]
-filename = "csv_files/7-14_test4.csv"
+step_size = 10
+filename = "csv_files/7-19_thor3.csv"
 
 def main(filename, readings_per_batch, max_voltage, trials, wait_time,
-            pause, offset, voltages=False, area = None, separation = None, plot = True):
+            pause, offset, voltages=False, step_size = 10, area = None, separation = None, plot = True):
         Collect_Data(filename, readings_per_batch, max_voltage, trials,
                 wait_time, pause, offset, voltages)
-        Shear_Distance(filename, area, separation, plot)
+        Shear_Distance(filename, step_size, area, separation, plot)
 
 main(filename=filename, readings_per_batch=readings_per_batch,
         max_voltage=max_voltage, trials=trials, wait_time=wait_time,
-        pause=True, offset=225, voltages=False, plot = False)
+        pause=True, offset=offset, voltages=False, plot = True)
