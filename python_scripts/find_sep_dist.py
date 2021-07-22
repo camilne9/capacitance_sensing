aa7e0a9672c045be875685b249d62f2aa9d9ebd8
= <=>
2021-07-22 07:47:01 -0400

    step size argument added

--- python_scripts/find_sep_dist.py
+++ python_scripts/find_sep_dist.py
@@ -7,6 +7,8 @@ import os.path
 from os import path
 import scipy.optimize as optimize
 import matplotlib.pyplot as plt
+import seaborn
+import pandas
 
 
 class Shear_Distance():
@@ -14,25 +16,28 @@ class Shear_Distance():
     Class for calculating shear distance, overlapping area, and initial
     separation distance.
     """
-    def __init__(self, filename, area = None, separation = None, plot = True):
+    def __init__(self, filename, step_size = 10, area = None, separation = None, plot = True):
         """
         Executes appropriate methods in the correct order. Takes one required
         argument which must be the name of the csv file containing the relevant
-        data and one optional argument to indicate if you want the data plotted.
+        data and several optional arguments to indicate the step size (in
+        microns), the area (in mm^2) if you know it, the initial separation
+        distance (in microns) if you know it, and whether you want the data
+        plotted.
         """
         print("init analyze")
         self.ep = 8.85
+        self.step_size = step_size
         # creates variable "self.data" that is a list of lists containing
         # all of the information from the named csv file
         self.data = self.avg_data(self.load_csv(filename))
         # creates variables "self.area" and "self.separation" which indicate
         # the error minimizing overlapping area and initial electrode
         # separation distance respectively unless values are already passed
-        if area == None or separation == None:
-            self.area, self.separation = self.find_area_and_sep()
-        else:
-            self.area = area
-            self.separation = separation
+        self.area = area
+        self.separation = separation
+        if area is None or separation is None:
+            self.find_area_and_sep()
         # creates variable for the error minimizing shear distance by taking
         # the area and separation distance as given
         self.shear_distance = self.find_shear_distance()
@@ -68,7 +73,9 @@ class Shear_Distance():
             for row in data_array:
                 if int(int(10*float(row[0]))/10) == batch:
                     temp_filtered_array.append(row)
-            avg_data.append(np.mean(np.array(temp_filtered_array), axis = 0).tolist())
+            if temp_filtered_array != []:
+                avg_data.append(np.mean(np.array(temp_filtered_array), axis = 0).tolist())
+        print(avg_data)
         return avg_data
 
 
@@ -82,14 +89,20 @@ class Shear_Distance():
         # filters the data for only cases were the stack was NOT sheared
         zero_voltage = []
         for row in self.data:
-            if row[1] == 0:
+            if row[1] == 0 and row[0]%2 == 0:
                 zero_voltage.append(row)
-
         area, separation = params
+        if self.area is not None:
+            area = self.area
+        if self.separation is not None:
+            separation = self.separation
         # sums the squared error for each of the sheared cases
         error = 0
         for row in zero_voltage:
-            error += (row[-1]-self.ep*area/(separation+5*row[0]))**2
+            # row[-1] is the experimental capacitance and row[0] is the batch
+            # number which implictly tells the separation distance relative to
+            # the initial separation
+            error += (row[-1]-self.ep*area/(separation+self.step_size/2*row[0]))**2
         return error
 
     def shear(self, x):
@@ -122,7 +135,14 @@ class Shear_Distance():
         # an error message
         if result.success:
             fitted_params = result.x
-            return fitted_params
+            # I only overwrite the area and separation if they were initially
+            # not passed
+            if self.area is None:
+                self.area = fitted_params[0]
+            if self.separation is None:
+                self.separation = fitted_params[1]
+            area_sep = [self.area, self.separation]
+            return area_sep
         else:
             raise ValueError(result.message)
 
@@ -160,8 +180,25 @@ class Shear_Distance():
         the last (which is the format done by collect_data.py)
         """
         print("plotting...")
-        plt.scatter(np.array(data_array)[:, 0].tolist(), np.array(data_array)[:, -1].tolist())
+        df = pandas.DataFrame({"batch":np.array(data_array)[:, 0].tolist(),
+                                "Voltage":np.array(data_array)[:, 1].tolist(),
+                                "time":np.array(data_array)[:, 2].tolist(),
+                                "raw":np.array(data_array)[:, 3].tolist(),
+                                "capacitance":np.array(data_array)[:, 4].tolist()})
+        fg = seaborn.FacetGrid(data=df, hue='Voltage', aspect=1.61)
+        fg.map(plt.scatter, 'time', 'capacitance').add_legend()
+        plt.suptitle("Capacitance over Time", fontsize = 14)
+        plt.title("D255 offset")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Capacitance (pF)")
+        plt.subplots_adjust(top=.85)
         plt.show()
+
+        # (ggplot(aes(x = 'time', y = 'capacitance', color = 'voltage'), data = df) +
+        # geom_point())
+        # plt.scatter(np.array(data_array)[:, 0].tolist(), np.array(data_array)[:, -1].tolist())
+        # plt.title("Capacitance Over Time")
+        # plt.show()
 
 # def main(filename, area = None, separation = None, plot = True):
 #     print("here2")
@@ -169,9 +206,9 @@ class Shear_Distance():
 #
 # main("csv_files/7-13_5m8.csv", plot = False)
 
-filename = "csv_files/7-13_5m8.csv"
+filename = "csv_files/7-19_thor3.csv"
 
 
 if __name__ == "__main__":
     print("inside if (analyze)")
-    Shear_Distance(filename, plot = False)
+    Shear_Distance(filename, area = None, plot = True)
